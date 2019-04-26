69bda9d8daba16ad82f70e278ece2e5fc4bdd79b
고경재 <kyronko@naver.com>
2019-04-26 20:08:53 +0900

    클릭시 나오는 화면생성

---
+++ app/src/main/java/com/example/jickbangviewhw/HouseDetail.java
@@ -0,0 +1,13 @@
+package com.example.jickbangviewhw;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HouseDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_house_detail);
+    }
+}
