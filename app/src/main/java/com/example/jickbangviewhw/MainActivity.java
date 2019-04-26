d6450eaae7a1d3b8ff779585b602cacfd596e9c6
고경재 <kyronko@naver.com>
2019-04-26 19:37:39 +0900

    입력정보값, 글자크기 수정

--- app/src/main/java/com/example/jickbangviewhw/MainActivity.java
+++ app/src/main/java/com/example/jickbangviewhw/MainActivity.java
@@ -27,12 +27,12 @@ public class MainActivity extends AppCompatActivity {
 
     }
     void fullhouse(){
-        HouseList.add(new HouseData("100/13",2,"경상북도 구미시 송정동","(풀옵션 초대박원룸)번개시장근..."));
-        HouseList.add(new HouseData("100/15",3,"경상북도 구미시 신평동","(풀옵션 초대박원룸)번개시장근..."));
-        HouseList.add(new HouseData("100/13",2,"경상북도 구미시 송정동","(풀옵션 초대박원룸)번개시장근..."));
-        HouseList.add(new HouseData("100/13",2,"경상북도 구미시 송정동","(풀옵션 초대박원룸)번개시장근..."));
-        HouseList.add(new HouseData("100/13",2,"경상북도 구미시 송정동","(풀옵션 초대박원룸)번개시장근..."));
-        HouseList.add(new HouseData("100/13",2,"경상북도 구미시 송정동","(풀옵션 초대박원룸)번개시장근..."));
+        HouseList.add(new HouseData("100/13",2,"경상북도 구미시 송정동","번개시장"));
+        HouseList.add(new HouseData("100/15",3,"경상북도 구미시 신평동","신평1동사무소부근"));
+        HouseList.add(new HouseData("100/13",2,"경상북도 구미시 송정동","구미역뒤_모든"));
+        HouseList.add(new HouseData("100/13",2,"경상북도 구미시 송정동","먹자골목근처"));
+        HouseList.add(new HouseData("100/13",2,"경상북도 구미시 송정동","새출발을 응원합니다"));
+
 
 
     }
